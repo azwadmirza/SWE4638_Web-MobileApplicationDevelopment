b047e8aea031e5ec3b1c9f0706ba4024b84cc317
Mirza Mohammad Azwad <86553976+azwadmirza@users.noreply.github.com>
2024-04-09 21:37:25 +0600

    Update App.jsx

--- Lab03/src/App.jsx
+++ Lab03/src/App.jsx
@@ -1,15 +1,13 @@
-// import Timer from './components/Timer';
-// import ToDo from './components/ToDo';
+import Timer from './components/Timer';
+import ToDo from './components/ToDo';
 import './App.css'
-import ToDoFunctional from './components/functional/ToDo';
-import TimerFunctional from './components/functional/Timer';
 
 function App() {
 
   return (
     <>
-      <TimerFunctional locale="en-US"/>
-      <ToDoFunctional/>
+      <Timer locale="en-US"/>
+      <ToDo/>
     </>
   )
 }
