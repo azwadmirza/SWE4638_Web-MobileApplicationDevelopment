c621da3344093b961994111f9ba7181bda76791b
azwadmirza <mirzaazwad@iut-dhaka.edu>
2024-04-15 10:57:23 +0600

    Lab 4

--- Lab04/app/components/question-answer.js
+++ Lab04/app/components/question-answer.js
@@ -6,11 +6,12 @@ import Timer from "./timer";
 
 const QuestionAnswer = ({ id }) => {
     const [quiz,setQuiz]=useState(null);
+    const [selectedAnswers, setSelectedAnswers] = useState(null);
     useEffect(()=>{
         const quiz = JSON.parse(localStorage.getItem("quiz"))[id];
+        setSelectedAnswers(Array(quiz.questions.length).fill(null));
         setQuiz(quiz);
     },[])
-    const [selectedAnswers, setSelectedAnswers] = useState(Array(quiz.questions.length).fill(null));
     const [submitted, setSubmitted] = useState(false);
     const [score, setScore] = useState(0);
     const [time,setTime]=useState({hours:0,minutes:0,seconds:0});
@@ -18,8 +19,10 @@ const QuestionAnswer = ({ id }) => {
     const [progress, setProgress] = useState(0);
 
     useEffect(()=>{
-        const selected = selectedAnswers.filter(answer => answer === null);
-        setProgress((quiz.questions.length-selected.length) / quiz.questions.length * 100);
+        if(selectedAnswers){
+            const selected = selectedAnswers.filter(answer => answer === null);
+            setProgress((quiz.questions.length-selected.length) / quiz.questions.length * 100);
+        }
     },[selectedAnswers,quiz])
 
     const handleAnswerChange = (index, value) => {
@@ -47,54 +50,59 @@ const QuestionAnswer = ({ id }) => {
         localStorage.setItem("results", JSON.stringify(results));
     };
 
-    return (
-        <div className="mt-6 mb-6">
-            <Timer setTimeTaken={setTimeTaken} initialTime={quiz.time} submitted={submitted} submitQuiz={submitQuiz}/>
-
-            <div className="w-full bg-white rounded-full h-3 dark:bg-gray-700">
-                <div className="bg-blue-600 h-3 rounded-full" style={{width:`${progress}%`}}></div>
+    if(quiz){
+        return (
+            <div className="mt-6 mb-6">
+                <Timer setTimeTaken={setTimeTaken} initialTime={quiz.time} submitted={submitted} submitQuiz={submitQuiz}/>
+    
+                <div className="w-full bg-white rounded-full h-3 dark:bg-gray-700">
+                    <div className="bg-blue-600 h-3 rounded-full" style={{width:`${progress}%`}}></div>
+                </div>
+    
+                {submitted && (<div className="mt-2 mb-2 px-4 py-2 rounded font-bold text-white bg-green-500 border-green-900">Score: {score}/{quiz.questions.length} Time Taken: {`${timeTaken.hours}:${timeTaken.minutes}:${timeTaken.seconds}`}</div>)}
+                <ul>
+                    {quiz.questions.map((question, index) => (
+                        <div key={index} className="mt-2 mb-2">
+                            <h2 className="text-xl font-bold mb-2">
+                                {index + 1}{") "}{question.question}
+                            </h2>
+                            {question.options.map((option, optionIndex) => (
+                                <div key={optionIndex}>
+                                    <input
+                                        type="radio"
+                                        id={`${index}-${optionIndex}`}
+                                        name={`answer-${index}`}
+                                        value={option}
+                                        checked={selectedAnswers[index] === option}
+                                        onChange={() => handleAnswerChange(index, option)}
+                                        disabled={submitted}
+                                        className="mt-1 ms-2 mb-2"
+                                    />
+                                    <label htmlFor={`${index}-${optionIndex}`} className="ms-2">{option}</label>
+                                </div>
+                            ))}
+                            {submitted && (
+                                <div key={index} className="mt-6 mb-6">
+                                    {selectedAnswers[index] === question.answer
+                                        ? (<span className="bg-green-500 border-green-900 text-white rounded py-2 px-4">Correct !</span>)
+                                        : (<span className="bg-red-500 border-red-900 text-white rounded py-2 px-4">Wrong! Answer is {question.answer}</span>)}
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                </ul>
+                <button
+                    onClick={submitQuiz}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                    Submit Quiz
+                </button>
             </div>
-
-            {submitted && (<div className="mt-2 mb-2 px-4 py-2 rounded font-bold text-white bg-green-500 border-green-900">Score: {score}/{quiz.questions.length} Time Taken: {`${timeTaken.hours}:${timeTaken.minutes}:${timeTaken.seconds}`}</div>)}
-            <ul>
-                {quiz.questions.map((question, index) => (
-                    <div key={index} className="mt-2 mb-2">
-                        <h2 className="text-xl font-bold mb-2">
-                            {index + 1}{") "}{question.question}
-                        </h2>
-                        {question.options.map((option, optionIndex) => (
-                            <div key={optionIndex}>
-                                <input
-                                    type="radio"
-                                    id={`${index}-${optionIndex}`}
-                                    name={`answer-${index}`}
-                                    value={option}
-                                    checked={selectedAnswers[index] === option}
-                                    onChange={() => handleAnswerChange(index, option)}
-                                    disabled={submitted}
-                                    className="mt-1 ms-2 mb-2"
-                                />
-                                <label htmlFor={`${index}-${optionIndex}`} className="ms-2">{option}</label>
-                            </div>
-                        ))}
-                        {submitted && (
-                            <div key={index} className="mt-6 mb-6">
-                                {selectedAnswers[index] === question.answer
-                                    ? (<span className="bg-green-500 border-green-900 text-white rounded py-2 px-4">Correct !</span>)
-                                    : (<span className="bg-red-500 border-red-900 text-white rounded py-2 px-4">Wrong! Answer is {question.answer}</span>)}
-                            </div>
-                        )}
-                    </div>
-                ))}
-            </ul>
-            <button
-                onClick={submitQuiz}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-                Submit Quiz
-            </button>
-        </div>
-    );
+        );
+    }
+    else{
+        return null;
+    }
 };
 
 export default QuestionAnswer;
